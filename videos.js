acb3a7ab1541a90b545a59cf8fc9d8c63ded6376
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2025-09-29 03:33:49 -0500

    unreal

--- videos.js
+++ videos.js
@@ -316,6 +316,15 @@ function coding(){
 
     <div class="video2">
     <h1>
+        Eye in the Sky
+    </h1>
+    <div class="vid2">
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/9YfSRRbZHQ4?si=--69_D4FFtRL6nmi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+</div>
+
+    <div class="video2">
+    <h1>
         Shock Rifle (chain reaction explosions)
     </h1>
     <div class="vid2">
